94eff181f2121ee379efdc433fa49a8c3bc5a01f
kailaaaputrii <kailaputri0509@gmail.com>
2024-11-19 12:51:20 +0700

    Add files via upload

---
+++ NO 3 PROGRAM MENGHITUNG SEGITIGA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+static void Main()
+{
+// Minta pengguna memasukkan nilai alas
+Console.Write("Masukkan nilai alas segitiga: ");
+double alas = Convert.ToDouble(Console.ReadLine());
+// Minta pengguna memasukkan nilai tinggi
+Console.Write("Masukkan nilai tinggi segitiga: ");
+double tinggi = Convert.ToDouble(Console.ReadLine());
+// Hitung luas segitiga
+double luas = 0.5 * alas * tinggi;
+// Tampilkan hasil luas segitiga
+Console.WriteLine("Luas segitiga adalah: " + luas);
+}
+}
+
+
+
---
+++ NO 4 PROGRAM MENGHITUNG LUAS JAJARGENJAR.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+static void Main()
+{
+// Minta pengguna memasukkan nilai alas
+Console.Write("Masukkan nilai jajargenjang: ");
+double alas = Convert.ToDouble(Console.ReadLine());
+// Minta pengguna memasukkan nilai tinggi
+Console.Write("Masukkan nilai tinggi jajargenjar: ");
+double tinggi = Convert.ToDouble(Console.ReadLine());
+// Hitung luas segitiga
+double luas = alas * tinggi;
+// Tampilkan hasil luas segitiga
+Console.WriteLine("Luas jajargenjang adalah: " + luas);
+}
+}
